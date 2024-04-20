caa072d251ad21b2a3a1bb75e6ee9b3876145cf4
miya1007hiro1999 <miya1007hiro1999@gmail.com>
2024-04-20 18:33:18 +0900

    classBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -3,4 +3,7 @@
         ヘッダー２
     </x-slot>
     コンポーネントテスト２
+
+    <x-test-class-base classBaseMessage="メッセージです"/>
+    <x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値を変えています"/>
 </x-tests.app>
