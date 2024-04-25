6b310b58d6421bc36e73c176c830b85fe9b3838f
miya1007hiro1999 <miya1007hiro1999@gmail.com>
2024-04-25 20:02:57 +0900

    途中

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\Owner; //エロくアント
 use Illuminate\Support\Facades\DB; //クエリビルダ
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
 
 class OwnersController extends Controller
 {
@@ -55,7 +57,20 @@ class OwnersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // $request->name;
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.Owner::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        Owner::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()->route('admin.owners.index');
     }
 
     /**
--- resources/views/admin/owners/create.blade.php
+++ resources/views/admin/owners/create.blade.php
@@ -16,50 +16,73 @@
                                 </h1>
                             </div>
                             <div class="lg:w-1/2 md:w-2/3 mx-auto">
-                                <div class=" -m-2">
-                                    <div class="p-2 w-1/2 mx-auto">
-                                        <div class="relative">
-                                            <label for="name" class="leading-7 text-sm text-gray-600">オーナー名</label>
-                                            <input type="text" id="name" name="name" required
-                                                class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                        </div>
+
+                                @if ($errors->any())
+                                    <div class="error-index text-red-600">
+                                        <ul>
+                                            @foreach ($errors->all() as $error)
+                                                <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
                                     </div>
-                                    <div class="p-2 w-1/2 mx-auto">
-                                        <div class="relative">
-                                            <label for="email" class="leading-7 text-sm text-gray-600">メールアドレス</label>
-                                            <input type="email" id="email" name="email" required
-                                                class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                @endif
+
+                                {{-- <x-input-error :messages="$errors->get()" class="mt-2" /> --}}
+                                <form method="post" action="{{ route('admin.owners.store') }}">
+                                    @csrf
+                                    <div class=" -m-2">
+                                        <div class="p-2 w-1/2 mx-auto">
+                                            <div class="relative">
+                                                <label for="name"
+                                                    class="leading-7 text-sm text-gray-600">オーナー名</label>
+                                                <input type="text" id="name" name="name" required
+                                                    value="{{ old('name') }}"
+                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="p-2 w-1/2 mx-auto">
-                                        <div class="relative">
-                                            <label for="password" class="leading-7 text-sm text-gray-600">パスワード</label>
-                                            <input type="password" id="password" name="password" required
-                                                class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                        <div class="p-2 w-1/2 mx-auto">
+                                            <div class="relative">
+                                                <label for="email"
+                                                    class="leading-7 text-sm text-gray-600">メールアドレス</label>
+                                                <input type="email" id="email" name="email" required
+                                                    value="{{ old('email') }}"
+                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="p-2 w-1/2 mx-auto">
-                                        <div class="relative">
-                                            <label for="password_confimation" class="leading-7 text-sm text-gray-600">パスワード確認</label>
-                                            <input type="passwordl" id="password_confimation" name="password_confimation" required
-                                                class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                        <div class="p-2 w-1/2 mx-auto">
+                                            <div class="relative">
+                                                <label for="password"
+                                                    class="leading-7 text-sm text-gray-600">パスワード</label>
+                                                <input type="password" id="password" name="password" required
+                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="flex justify-around p-2 w-full mt-4">
-                                        <button
-                                        class=" text-white bg-indigo-500 border-0 py-4 px-12 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録</button>
-                                        <button
-                                        onclick="location.href='{{ route('admin.owners.index' )}}'"    class=" text-black bg-gray-500 border-0 py-4 px-12 focus:outline-none hover:bg-gray-700 rounded text-lg">戻る</button>
-                                    
+                                        <div class="p-2 w-1/2 mx-auto">
+                                            <div class="relative">
+                                                <label for="password_confirmation"
+                                                    class="leading-7 text-sm text-gray-600">パスワード確認</label>
+                                                <input type="password" id="password_confirmation"
+                                                    name="password_confirmation" required
+                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                            </div>
                                         </div>
-                                    
+                                        <div class="flex justify-around p-2 w-full mt-4">
+                                            <button type="submit"
+                                                class=" text-white bg-indigo-500 border-0 py-4 px-12 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録</button>
+                                            <button type="button"
+                                                onclick="location.href='{{ route('admin.owners.index') }}'"
+                                                class=" text-black bg-gray-500 border-0 py-4 px-12 focus:outline-none hover:bg-gray-700 rounded text-lg">戻る</button>
+
+                                        </div>
+
                                     </div>
-                                </div>
                             </div>
+                            </form>
                         </div>
-                    </section>
                 </div>
+                </section>
             </div>
         </div>
+    </div>
     </div>
 </x-app-layout>
