7f5347329807d0ae17577f5bdf469fa60c2be508
miya1007hiro1999 <miya1007hiro1999@gmail.com>
2024-04-25 18:34:51 +0900

    Carbon

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Owner; //エロくアント
+use Illuminate\Support\Facades\DB; //クエリビルダ
+use Carbon\Carbon;
 
 class OwnersController extends Controller
 {
@@ -19,7 +22,22 @@ class OwnersController extends Controller
     }
     public function index()
     {
-        dd('test');
+        $date_now = Carbon::now();
+        $date_parse = Carbon::parse(now());
+        echo $date_now->year;
+        
+        echo $date_parse;
+
+        $e_all =Owner::all();
+        $q_get = DB::table('owners')->select('name','created_at')->get();
+        $q_first = DB::table('owners')->select('name')->first();
+        $c_test = collect([
+            'name'=>'test',
+        ]);
+
+        var_dump($q_first);
+        // dd($e_all,$e_get,$q_first,$c_test);
+        return view('admin.owners.index', compact('e_all','q_get'));
     }
 
     /**
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,9 +18,9 @@
         <div class="min-h-screen bg-gray-100">
             @if(auth('admin')->user())
                 @include('layouts.admin-navigation')
-            @elseif(auth('owner')->user())
+            @elseif(auth('owners')->user())
                 @include('layouts.owner-navigation')    
-            @elseif(auth('user')->user())
+            @elseif(auth('users')->user())
                 @include('layouts.user-navigation') 
             @endif
                 
