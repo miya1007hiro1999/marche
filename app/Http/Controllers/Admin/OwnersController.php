982370fae8c3e7f70b037e957727cf11221bc3fe
miya1007hiro1999 <miya1007hiro1999@gmail.com>
2024-04-26 09:31:38 +0900

    store

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -70,7 +70,9 @@ class OwnersController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        return redirect()->route('admin.owners.index');
+        return redirect()
+        ->route('admin.owners.index')
+        ->with('message','登録を実施しました');
     }
 
     /**
