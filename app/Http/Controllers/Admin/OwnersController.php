0cf3ac7ec0af0910f144697e5fa986152e501ec4
miya1007hiro1999 <miya1007hiro1999@gmail.com>
2024-04-27 00:35:47 +0900

    edit

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -40,7 +40,7 @@ class OwnersController extends Controller
         // var_dump($q_first);
         // dd($e_all,$e_get,$q_first,$c_test);
 
-        $owners = Owner::select('name','email','created_at',)->get();
+        $owners = Owner::select('name','email','created_at','id')->get();
         return view('admin.owners.index', compact('owners'));
     }
 
@@ -88,7 +88,8 @@ class OwnersController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+        return view('admin.owners.edit' , compact('owner'));
     }
 
     /**
@@ -96,7 +97,17 @@ class OwnersController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+        $owner->name = $request->name;
+        $owner->email = $request->email;
+        $owner->password = Hash::make($request->password);
+        $owner->save();
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with('message','オーナー情報を更新しました');
+
+
     }
 
     /**
