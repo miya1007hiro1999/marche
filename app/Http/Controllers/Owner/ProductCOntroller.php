2cee867d41ead092159247497748eb458b5838b2
miya1007hiro1999 <miya1007hiro1999@gmail.com>
2024-05-21 17:07:12 +0900

    eager loading

--- app/Http/Controllers/Owner/ProductCOntroller.php
+++ app/Http/Controllers/Owner/ProductCOntroller.php
@@ -4,15 +4,52 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Product;
+use App\Models\Image;
+use App\Models\SecondaryCategory;
+use App\Models\Owner;
+
 
 class ProductCOntroller extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    public static function middleware(): array
+    {
+        return [
+                'auth:owners',
+                function (Request $request, Closure $next) {
+                    // dd($next($request));
+                    $id= $request->route()->parameter('product');//shopのid取得
+                    if(!is_null($id)){
+                        $productsOwnerId = Product::findOrFail($id)->shop->owner->id;
+                        $productId = (int)$productsOwnerId;//キャスト 文字列→数値に変換
+                        if($productId !==  Auth::id()){
+                            abort(404);
+                        } 
+                    }
+                    return $next($request);
+                },
+        ];
+        
+    }
     public function index()
     {
-        //
+      //  $products = Owner::findOrFail(Auth::id())->shop->product;
+
+        $ownerInfo = Owner::with('shop.product.imageFirst')
+        ->where('id',Auth::id())->get();
+
+        // dd($ownerInfo);
+        // foreach($ownerInfo as $owner){
+        //     foreach($owner->shop->product as $product){
+        //         dd($product->imageFirst->filename);
+        //     }
+        //     // dd($owner->shop->product);
+        // }
+
+
+        return view('owner.products.index',compact('ownerInfo'));
     }
 
     /**
