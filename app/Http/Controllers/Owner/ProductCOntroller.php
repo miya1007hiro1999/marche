9390edc00c4dd6985b72906191de4a393205cbd7
miya1007hiro1999 <miya1007hiro1999@gmail.com>
2024-06-28 19:16:00 +0900

    product destroy

--- app/Http/Controllers/Owner/ProductCOntroller.php
+++ app/Http/Controllers/Owner/ProductCOntroller.php
@@ -212,6 +212,12 @@ class ProductController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        
+        Product::findOrFail($id)->delete();
+
+        return redirect()
+        ->route('owner.products.index')
+        ->with(['message'=>'画像を削除しました',
+    'status'=>'alert']);
     }
 }
