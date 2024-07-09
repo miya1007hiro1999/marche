ed2391731316f521e68844b7ec522d3228cecc4f
miya1007hiro1999 <miya1007hiro1999@gmail.com>
2024-07-09 19:51:27 +0900

    商品情報の在庫

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Stock;
 
 class ItemController extends Controller
 {
@@ -66,6 +67,13 @@ class ItemController extends Controller
     {
         $product = Product::findOrFail($id);
 
-        return view('user.show', compact('product'));
+        $quantity =  Stock::where('product_id' , $product->id)
+        ->sum('quantity');
+
+        if($quantity > 9){
+            $quantity =9;
+        }
+
+        return view('user.show', compact('product' , 'quantity'));
     }
 }
