575ed09eec1c049e8668dd1a7c73d2e9daeb7a2b
miya1007hiro1999 <miya1007hiro1999@gmail.com>
2024-07-10 14:11:47 +0900

    cart index

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -6,11 +6,26 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Cart;
+use App\Models\User;
 
 class CartController extends Controller
 {
     
+    public function index()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+        $totalPrice = 0;
 
+        foreach($products as $product){
+          $totalPrice =   $product->price * $product->pivot->quantity;
+        }
+
+
+        return view('user.cart',
+        compact('products','totalPrice'));
+
+    }
     public function add(Request $request)
     {
         $itemInCart = Cart::where('product_id' , $request->product_id)
@@ -27,6 +42,7 @@ class CartController extends Controller
                 'quantity' => $request->quantity,
             ]);
         }
-                dd('テスト');
+        
+        return redirect()->route('user.cart.index');
     }
 }
