acd065dba59a5423034cb4b4942ac740e0b9a638
miya1007hiro1999 <miya1007hiro1999@gmail.com>
2024-07-12 18:45:15 +0900

    form

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -39,27 +39,27 @@ class Product extends Model
 
     public function category()
     {
-        return $this->belongsTo(SecondaryCategory::class,'secondary_category_id');
+        return $this->belongsTo(SecondaryCategory::class, 'secondary_category_id');
     }
 
     public function imageFirst()
     {
-        return $this->belongsTo(Image::class,'image1','id');
+        return $this->belongsTo(Image::class, 'image1', 'id');
     }
 
     public function imageSecond()
     {
-        return $this->belongsTo(Image::class,'image2','id');
+        return $this->belongsTo(Image::class, 'image2', 'id');
     }
 
     public function imageThird()
     {
-        return $this->belongsTo(Image::class,'image3','id');
+        return $this->belongsTo(Image::class, 'image3', 'id');
     }
 
     public function imageFourth()
     {
-        return $this->belongsTo(Image::class,'image4','id');
+        return $this->belongsTo(Image::class, 'image4', 'id');
     }
 
 
@@ -71,7 +71,7 @@ class Product extends Model
     public function users()
     {
         return $this->belongsToMany(User::class, 'carts')
-        ->withPivot(['id','quantity']);
+            ->withPivot(['id', 'quantity']);
     }
 
 
@@ -87,57 +87,76 @@ class Product extends Model
             ->having('quantity', '>', 1);
 
         return $query
-        ->joinSub($stocks, 'stock', function ($join) {
-            $join->on('products.id', '=', 'stock.product_id');
-        })
-        ->join('shops', 'products.shop_id', '=', 'shops.id')
-        ->join(
-            'secondary_categories',
-            'products.secondary_category_id',
-            '=',
-            'secondary_categories.id'
-        )
-        ->join('images as image1', 'products.image1', '=', 'image1.id')
-        ->where('products.is_selling', true)
-        ->select(
-            'products.id as id',
-            'products.name as name',
-            'products.price'
-            ,
-            'products.sort_order as sort_order'
-            ,
-            'products.information',
-            'secondary_categories.name as category'
-            ,
-            'image1.filename as filename'
-        );
+            ->joinSub($stocks, 'stock', function ($join) {
+                $join->on('products.id', '=', 'stock.product_id');
+            })
+            ->join('shops', 'products.shop_id', '=', 'shops.id')
+            ->join(
+                'secondary_categories',
+                'products.secondary_category_id',
+                '=',
+                'secondary_categories.id'
+            )
+            ->join('images as image1', 'products.image1', '=', 'image1.id')
+            ->where('products.is_selling', true)
+            ->select(
+                'products.id as id',
+                'products.name as name',
+                'products.price'
+                ,
+                'products.sort_order as sort_order'
+                ,
+                'products.information',
+                'secondary_categories.name as category'
+                ,
+                'image1.filename as filename'
+            );
     }
 
     public function scopeSortOrder($query, $sortOrder)
     {
-        if($sortOrder === null || $sortOrder === Common::SORT_ORDER['recommend']){ 
-            return $query->orderBy('sort_order', 'asc') ; 
-            } 
-            if($sortOrder === Common::SORT_ORDER['higherPrice']){ 
-            return $query->orderBy('price', 'desc') ; 
-            } 
-            if($sortOrder === Common::SORT_ORDER['lowerPrice']){ 
-            return $query->orderBy('price', 'asc') ; 
-            } 
-            if($sortOrder === Common::SORT_ORDER['later']){ 
-            return $query->orderBy('products.created_at', 'desc') ; 
-            } 
-            if($sortOrder === Common::SORT_ORDER['older']){ 
-            return $query->orderBy('products.created_at', 'asc') ; 
-            }
+        if ($sortOrder === null || $sortOrder === Common::SORT_ORDER['recommend']) {
+            return $query->orderBy('sort_order', 'asc');
+        }
+        if ($sortOrder === Common::SORT_ORDER['higherPrice']) {
+            return $query->orderBy('price', 'desc');
+        }
+        if ($sortOrder === Common::SORT_ORDER['lowerPrice']) {
+            return $query->orderBy('price', 'asc');
+        }
+        if ($sortOrder === Common::SORT_ORDER['later']) {
+            return $query->orderBy('products.created_at', 'desc');
+        }
+        if ($sortOrder === Common::SORT_ORDER['older']) {
+            return $query->orderBy('products.created_at', 'asc');
+        }
     }
 
-    public function scopeSelectCategory($query,$categoryId)
+    public function scopeSelectCategory($query, $categoryId)
     {
-        if($categoryId !== '0')
-        {
-            return $query->where('secondary_category_id',$categoryId);
-        }else{
+        if ($categoryId !== '0') {
+            return $query->where('secondary_category_id', $categoryId);
+        } else {
+            return;
+        }
+    }
+
+    public function scopeSearchKeyword($query, $keyword)
+    {
+        if (!is_null($keyword)) {
+            //全角スペースを半角に
+            $spaceConvert = mb_convert_kana($keyword, 's');
+
+            //空白で区切る
+            $keywords = preg_split('/[\s]+/', $spaceConvert, -1, PREG_SPLIT_NO_EMPTY);
+
+            //単語をループで回す
+            foreach ($keywords as $word) {
+                $query->where('products.name', 'like', '%' . $word . '%');
+            }
+
+            return $query;
+        } else {
             return;
         }
     }
